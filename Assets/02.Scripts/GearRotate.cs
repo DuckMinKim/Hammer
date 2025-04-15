b53c564f6d661ad19c6181544b4f9a75af1c87da
DuckMinKim <breadbag09@gmail.com>
2025-04-15 11:37:02 +0900

    기어추가(수정해야함)

---
+++ Assets/02.Scripts/GearRotate.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GearRotate : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed;
+    Rigidbody2D rb2;
+
+    private void Awake()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+        //transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        rb2.MoveRotation(rb2.rotation + rotateSpeed * Time.fixedDeltaTime);
+    }
+}
